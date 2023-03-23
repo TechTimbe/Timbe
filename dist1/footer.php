4f06fc7fef8e33d185427741de96617eace5d11d
TechTimbe <techtimbeservice@gmail.com>
2023-03-23 13:31:21 +0200

    Teste de AW

---
+++ dist1/footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Admincast bootstrap 4 &amp; angular 5 admin template, Шаблон админки | Dashboard</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link href="./assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="./assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
+    <!-- PLUGINS STYLES-->
+    <link href="./assets/vendors/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
+    <!-- THEME STYLES-->
+    <link href="assets/css/main.min.css" rel="stylesheet" />
+    <!-- PAGE LEVEL STYLES-->
+</head>
+
+<body class="fixed-navbar">
+    <div class="page-wrapper">
+        <!-- START HEADER-->
+       
+            <!-- END PAGE CONTENT-->
+            <footer class="page-footer">
+                <div class="font-13">2018 © <b>AdminCAST</b> - All rights reserved.</div>
+                <a class="px-4" href="http://themeforest.net/item/adminca-responsive-bootstrap-4-3-angular-4-admin-dashboard-template/20912589" target="_blank">BUY PREMIUM</a>
+                <div class="to-top"><i class="fa fa-angle-double-up"></i></div>
+            </footer>
+    </div>
+    <!-- BEGIN THEME CONFIG PANEL-->
+   
+</body>
+
+</html>
---
+++ dist1/header.php
@@ -0,0 +1,12 @@
+<?php
+include_once("home1.php");  ?>
+
+
+<div class="body flex-grow-1 px-3">
+    <div class="container-lg">
+        <h1> Conteudo da VIEW HOME</h1>
+    </div>
+</div>
+
+
+<?php include_once("footer.php");  ?>
